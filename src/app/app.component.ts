1878fd2a61b0ff899c6737e7f701e2d16f79fcc6
Jean <iepvjpereira@gmail.com>
2019-08-04 22:22:25 -0500

    Primer commit Angular Pruebas

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<app-header></app-header>`
+})
+export class AppComponent {
+  name = 'Hola world :v ';
+}
---
+++ src/app/components/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    template: `<h1>Header component</h1><img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Tipi_di_virgolette.svg/200px-Tipi_di_virgolette.svg.png" alt="imagen">`
+  })
+export class HeaderComponent {
+}
+
+
+
+
